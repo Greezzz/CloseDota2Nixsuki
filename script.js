89dfd10d616742501b9a8c9c9cf8c72d5e5b80d6
Greezzz <116001719+Greezzz@users.noreply.github.com>
2024-03-17 16:40:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ function getAnswer() {
     return;
   }
   
-  // Здесь вы можете добавить логику для поиска ответа в Интернете
-  // Например, вы можете использовать API для поиска или предварительно собранный список ссылок
+
   
-  // В данном примере просто генерируется фиктивная ссылка
+
   const answerContainer = document.getElementById('answer');
   answerContainer.innerHTML = `<p>Возможно, ответ на ваш вопрос можно найти <a href="https://www.example.com?q=${encodeURIComponent(question)}" target="_blank">здесь</a>.</p>`;
 }
